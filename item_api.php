6cbc08436d4657560754e6a29f6cfccc005c1a36
Sihan Bo <ccat-a@hotmail.com>
2017-10-22 19:55:09 -0700

    1. Created an associative array 'addPost' to store information from Post of adding actions.
    2. Use the array addPost as the parameter instead of 5 string variables.
    
    3. Fixes the error that the price could not be inserted to SQL.

--- item_api.php
+++ item_api.php
@@ -12,6 +12,7 @@ require('config.php');
 $jsonObj = new stdClass();
 $msg = null;
 $res_code = null;
+$addPost = null;
 // Success: 1 for Item Added. 2 for list all successfully.3 for Bid successfully. 4 for Mark as sold successfully.
 // Error: -1 for Item failed to add. -2 for fail to list all. -3 for fail to bid. -4 for failed to Mark as sold.99 for wrong request format.
 
@@ -22,19 +23,29 @@ if (isset($_POST['api_key']) and $_POST['api_key'] == "hello") {
         and isset($_POST['description'])
         and isset($_POST['location'])
         and isset($_POST['exp_time'])
-        and isset($_POST['price'])) {
-        //Assigning posted values to variables.
+        and isset($_POST['price'])
+        and isset($_POST['image_url'])
+    ) {
+
+        //Assigning posted values to addPost Array.
+        foreach($_POST as $key => $value){
+            $addPost[$key] = $value;
+        }
+        //Checking if all the strings are legal sql code
+        foreach($addPost as $key => $value){
+            $value = $conn->real_escape_string($value);
+        }
+
+        /* clean the strings in the post, which is replaced by the above loop
         $item_name = $conn->real_escape_string($_POST['name']);
         $description = $conn->real_escape_string($_POST['description']);
         $location = $conn->real_escape_string($_POST['location']);
         $price = $conn->real_escape_string($_POST['price']);
-        if (isset($_POST['image_url'])) {
-            $image_url = $conn->real_escape_string($_POST['image_url']);
-        }
-
+        $image_url = $conn->real_escape_string($_POST['image_url']);
         $exp_time = $conn->real_escape_string($_POST['exp_time']);
+        */
 
-        list($res_code, $msg) = addItem($item_name, $description, $location, $exp_time, $conn);
+        list($res_code, $msg) = addItem($addPost, $conn);
 
     } elseif ($_POST['action'] == "listAll") {
         $query = "SELECT * FROM `items`";
@@ -127,21 +138,27 @@ echo json_encode($jsonObj);
  * @param $conn mysqli
  * @return array
  */
-function addItem($item_name, $description, $location, $exp_time, $conn)
+function addItem($addPost, $conn)
 {
+    $item_name = $addPost['name'];
+    $description = $addPost['description'];
+    $location = $addPost['location'];
+    $price = $addPost['price'];
+    $image_url = $addPost['image_url'];
+    $exp_time = $addPost['exp_time'];
 //Build and execute the INSERT queue
-    $query = "INSERT INTO `items`  (`name`, `description`, `location`, `exp_time`) VALUES ('$item_name', '$description', '$location','$exp_time')";
+    $query = "INSERT INTO `items`  (`name`, `description`, `location`, `price`,`exp_time`) VALUES ('$item_name', '$description', '$location','$price','$exp_time')";
 
     $result = $conn->query($query) or $msg = $conn->error;
     $count = $conn->affected_rows;
     //If the posted values are equal to the database values, then result will be 1.
     if ($count == 1) {
         $msg = "Item added successfully.";
-        $result = 1;
+        $res_code = 1;
     } else {
         //If the login credentials doesn't match, he will be shown with an error message.
-        $msg = "Failed to add item";
-        $result = -1;
+        $msg = $conn->error;
+        $res_code = -1;
     }
-    return array($result, $msg);
+    return array($res_code, $msg);
 }
